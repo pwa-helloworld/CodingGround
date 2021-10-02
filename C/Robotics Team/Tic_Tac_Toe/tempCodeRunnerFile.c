7778d5272b879f4c033cc3860dae62da6ae128a6
tommylhw <62752366+tommylhw@users.noreply.github.com>
2021-10-02 22:21:44 +0800

    Tic tac toe

---
+++ C/Robotics Team/Tic_Tac_Toe/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+else {
+        //     sign = 'x';
+
+        //     printf("Player 2 term! \n");
+        //     printf("Please enter your position: ");
+        //     scanf("%d", pos);
+
+        //     square[pos] = sign;
+        // }
+        // printf("Test");
