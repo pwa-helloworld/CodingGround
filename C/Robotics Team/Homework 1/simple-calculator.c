4453b9d425a6e889788368e0375a30958d7882a1
tommylhw <62752366+tommylhw@users.noreply.github.com>
2021-10-10 22:01:59 +0800

    1

--- C/Robotics Team/Homework 1/simple-calculator.c
+++ C/Robotics Team/Homework 1/simple-calculator.c
@@ -61,87 +61,67 @@
 #include <string.h>
 #include<stdlib.h>
 
-float x, y;
+float firf, secf;
 char f;
 char opt;
-char k[100], str[100], e[100], d[100];
+char k[100], str[100], firInt[100], secInt[100];
 int i,j,z;
 int a,b,c = 0;
 char *store;
+
 int main(void) {
-  // scanf("%f", &x);
-  // scanf("%c", &opt);
-  // scanf("%f", &y);
-  // printf("%c", opt);
 
   scanf("%s", str);
-  // printf("%s", str);
-  for (i = 0; i < 100; i++)
-  {
-   
-  //   e[i] = str[i];
-  //   printf("%d", a);
 
-  // }
-  // if(str[i])
-  // {;
-  // }
+  for (i = 0; i < 100; i++) {
  
  
-  if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
-  {
-  // for(i = 0; i < ; i++)
-    if(str[i] == '+')
-    {
+ 
+  if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+
+    if(str[i] == '+') {
       f = '+';
     }
-    if(str[i] == '-')
-    {
+    if(str[i] == '-') {
       f = '-';
     }
-    if(str[i] == '*')
-    {
+    if(str[i] == '*') {
       f = '*';
     }
-    if(str[i] == '/')
-    {
+    if(str[i] == '/') {
       f = '/';
     }
     a = i;
 
   }
-  }
+}
 
  
-  for (j = 0; j < a; j++)
-  {
-    e[j] = str[j];
+  for (j = 0; j < a; j++) {
+    firInt[j] = str[j];
   }
  
-  for (z = a+1; z < 100; z++)
-  {
-    d[z-a-1] = str[z];
+  for (z = a+1; z < 100; z++) {
+    secInt[z-a-1] = str[z];
   }
-  // printf("%s", e);
-  b = atoi(e);
-  c = atoi(d);
-  x = b;
-  y = c;
-  if(f == '+')
-    {
-      printf("%f", x+y);
-    }
-  if(f == '-')
-  {
-    printf("%f", x-y);
+
+  b = atoi(firInt);
+  c = atoi(secInt);
+  firf = b;
+  secf = c;
+  if(f == '+') {
+      printf("%f", firf + secf);
   }
-  if(f == '*')
-  {
-    printf("%f", x*y);
+  if(f == '-') {
+    printf("%f", firf - secf);
   }
-  if(f == '/')
-  {
-    printf("%f", x/y);
+  if(f == '*') {
+    printf("%f", firf * secf);
   }
+  if(f == '/') {
+    printf("%f", firf / secf);
+  }
+
+  return 0;
 
 }
