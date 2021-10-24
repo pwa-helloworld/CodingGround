ca661a4c9beece3cf784fa8219bddefe6769f154
tommylhw <62752366+tommylhw@users.noreply.github.com>
2021-10-24 23:41:59 +0800

    1

--- Python/test/test.py
+++ Python/test/test.py
@@ -1,10 +1,16 @@
-x = 5
+# x = 5
 
-def foo():
-  global x
-  x = x - 1
-  print("x inside:", x)
+# def foo():
+#   global x
+#   x = x - 1
+#   print("x inside:", x)
 
 
-foo()
-print("x outside:", x)
+# foo()
+# print("x outside:", x)
+
+list = ["a", "b"]
+
+# t = list[0]
+
+print(list[0:2])
