8dfd102c04fa8173a1c207a5d2b0dbefd4e5c55a
tommylhw <62752366+tommylhw@users.noreply.github.com>
2022-01-15 20:45:31 +0800

    1

--- Python/Turtle/first-turtle.py
+++ Python/Turtle/first-turtle.py
@@ -3,14 +3,15 @@ import turtle
 turtle.getscreen()
 turtle.speed(10)
 
-for j in range (20):
-  
-  for i in range (4):
+for i in range (20):
+  if (i % 2) == 0:
+    turtle.color('blue')
+  else:
     turtle.color('red')
+    
+  for j in range (4):
     turtle.fd(100)
     turtle.rt(90)
-    
-  turtle.color('blue')
   turtle.rt(18)
 
 
