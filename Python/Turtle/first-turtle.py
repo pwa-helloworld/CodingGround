ec78f6c439123a3e782cb6d6bf87f3c4d51f492b
Tommy Working Copy <lokhim128@gmail.com>
2022-01-15 20:31:34 +0800

    16

--- Python/Turtle/first-turtle.py
+++ Python/Turtle/first-turtle.py
@@ -3,20 +3,10 @@ import turtle
 turtle.getscreen()
 
 for j in range (10):
-for i in range (4):
-turtle.fd(100)
-turtle.rt(90)
-turtle.rt(36)
-	
-	
-	
-	
-	
+	for i in range (4):
+	  turtle.fd(100)
+	  turtle.rt(90)
+	turtle.rt(36)
 
-# turtle.fd(100)
-# turtle.rt(90)
-# turtle.fd(100)
-# turtle.rt(90)
-# turtle.fd(100)
 
 turtle.done()
