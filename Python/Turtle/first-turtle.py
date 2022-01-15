56b2fe872e759cd4b5510c364ed4d3d91cebcd29
Tommy Working Copy <lokhim128@gmail.com>
2022-01-15 20:38:51 +0800

    2

--- Python/Turtle/first-turtle.py
+++ Python/Turtle/first-turtle.py
@@ -4,9 +4,11 @@ turtle.getscreen()
 turtle.speed(10)
 
 for j in range (20):
+	turtle.color('red')
   for i in range (4):
     turtle.fd(100)
     turtle.rt(90)
+  turtle.color('blue')
   turtle.rt(18)
 
 
