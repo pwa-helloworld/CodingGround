2bba4f5b1ac999dc0e3254a8d4973851c6095f39
tommylhw <62752366+tommylhw@users.noreply.github.com>
2022-01-18 18:23:01 +0800

    1

--- Python/PyGame/first-pygame.py
+++ Python/PyGame/first-pygame.py
@@ -3,13 +3,16 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
-
 running = True
 while running:
   for event in pygame.event.get():
     # print(event)
     if event.type == pygame.QUIT:
       running = False
+
+
+
+
 
 
 pygame.quit()
