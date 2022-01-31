4deadd106821fe613ccccc8e9590fc39211a9972
tommylhw <62752366+tommylhw@users.noreply.github.com>
2022-01-31 20:55:34 +0800

    1

---
+++ Web Design/My Website 2/js/tabbed-content.js
@@ -0,0 +1,19 @@
+window.onload = (event) => {
+  OpenSection('About');
+  // document.getElementById('AboutButton').style.background = "var(--light-blue);";
+};
+
+function OpenSection(topicName) {
+  var i;
+  var topicID = document.getElementsByClassName('topic-section');
+  var btnID = document.getElementsByClassName('menu-btn');
+
+  for (i = 0; i < topicID.length; i++) {
+    topicID[i].style.display = "none";
+    // btnID[i].style.background = "var(--dark-blue)";
+  }
+
+  document.getElementById(topicName).style.display = "block";
+  // document.getElementById(topicName + "Button").style.display = "var(--light-blue);"
+
+}
